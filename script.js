a2bea3498ac6e6f0e6a8370fc4fbc3599db9e765
Abdelrahman B <99421749+Abdelrahmanthecoder@users.noreply.github.com>
2023-09-27 08:15:35 +0300

    script.js updated

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+function trackMousePosition(event) {
+    // Get the position of the mouse.
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+  
+    // Update the position of the CSS gradient.
+    const gradient = document.querySelector('.gradient');
+    gradient.style.background = `linear-gradient(to right, red ${mouseX / window.innerWidth}%, green ${mouseY / window.innerHeight}%)`;
+}
+  
+// Add the event listener to the document.
+document.addEventListener('mousemove', trackMousePosition);
